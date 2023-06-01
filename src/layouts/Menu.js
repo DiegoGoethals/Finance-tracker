8066e895640ce10130f67e3f5b476ef0039167f1
diego <goethalsdiego@gmail.com>
2023-06-01 16:38:49 +0200

    Made it possible to open and close sidebar menu

--- src/layouts/Menu.js
+++ src/layouts/Menu.js
@@ -1,9 +1,18 @@
 import "./Menu.css";
 
 function Menu() {
+
+    const closeMenu = () => {
+        const menu = document.querySelector(".menu");
+        const icon = document.querySelector(".icon");
+        menu.style.opacity = "0";
+        menu.style.zIndex = "-1";
+        icon.style.opacity = "1";
+    }
+
     return (
         <aside className="menu">
-            <button>x</button>
+            <button onClick={closeMenu}>x</button>
             <p>Menu</p>
         </aside>
     );
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -4,9 +4,18 @@ import Menu from './Menu.js';
 
 function RootLayout() {
 
+  const toggleMenu = () => {
+    const menu = document.querySelector(".menu");
+    const icon = document.querySelector(".icon");
+    menu.style.opacity = "1";
+    menu.style.zIndex = "1";
+    icon.style.opacity = "0";
+  }
+
   return (
     <div className = "root">
       <p id="title">Finance Tracker</p>
+      <i className="fa-solid fa-bars icon" onClick={toggleMenu}></i>
       <Menu/>
       <main>
         <Outlet/>

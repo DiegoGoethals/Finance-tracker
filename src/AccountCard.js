9fa72f089c6cd9693f5d462941fd3c3d8bb738a3
diego <goethalsdiego@gmail.com>
2023-06-02 01:58:32 +0200

    Styled the account cards a bit more

--- src/AccountCard.js
+++ src/AccountCard.js
@@ -3,11 +3,18 @@ import "./AccountCard.css";
 function AccountCard(props) {
 
     const { account } = props;
+    const { name, balance } = account;
+    let balanceClass = "account-balance";
+    if (balance < 0) {
+        balanceClass += " negative";
+    }
 
     return (
         <div className="account-card">
-            <p>{account.name}</p>
-            <p>{account.balance}</p>
+            <div className="content-wrapper">
+                <p className="account-name">{name}</p>
+                <p className={balanceClass}>{balance}</p>
+            </div>
         </div>
     );
 }

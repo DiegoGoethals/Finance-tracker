043823963f3189723e5b5b8a757c8a9d541e02c3
diego <goethalsdiego@gmail.com>
2023-06-02 16:18:36 +0200

    Close menu after clicking on a link

--- src/layouts/Menu.js
+++ src/layouts/Menu.js
@@ -2,9 +2,17 @@ import "./Menu.css";
 import { NavLink } from "react-router-dom";
 
 function Menu() {
+
+    const closeMenu = () => {
+        const menu = document.querySelector(".menu");
+        const icon = document.querySelector(".icon");
+        menu.style.transform = "translateX(-100%)";
+        icon.style.transform = "translateX(0)";
+      }
+
     return (
         <aside className="menu">
-            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/"} onClick={closeMenu}>Home</NavLink>
         </aside>
     );
 }

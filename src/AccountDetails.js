d5947bedab197702176b991eef14469e1bb405ef
diego <goethalsdiego@gmail.com>
2023-06-03 12:55:40 +0200

    Added a table to summarize the income and expenses of every month

--- src/AccountDetails.js
+++ src/AccountDetails.js
@@ -1,3 +1,4 @@
+import "./AccountDetails.css";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
@@ -5,19 +6,60 @@ function AccountDetails() {
     const { id } = useParams();
 
     const [account, setAccount] = useState(null);
+    const [months, setMonths] = useState([]); 
 
     useEffect(() => {
         fetch(`http://localhost:8000/accounts/${id}`)
             .then(response => response.json())
             .then(data => {
                 setAccount(data);
+                setMonths(data.months);
             });
     }, [id]);
 
     return (
-        <div>
+        <div className="details">
             <h1>Account Details</h1>
             {account && <><p>{account.name}</p><p>{account.balance}</p></>}
+            {months && <table>
+                <thead>
+                    <tr>
+                        <th>Month</th>
+                        <th>Income</th>
+                        <th>Expenses</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(months).map((month) => (
+                        <tr key={month}>
+                            <td>{month}</td>
+                            <td>
+                                {months[month].income.reduce(
+                                (accumulator, income) => accumulator + income.amount,
+                                    0
+                                    )}
+                            </td>
+                            <td>
+                                {months[month].expenses.reduce(
+                                (accumulator, expense) => accumulator + expense.amount,
+                                0
+                                )}
+                            </td>
+                            <td>
+                                {months[month].income.reduce(
+                                (accumulator, income) => accumulator + income.amount,
+                                0
+                                ) -
+                                months[month].expenses.reduce(
+                                    (accumulator, expense) => accumulator + expense.amount,
+                                    0
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>}
         </div>
     );
 }

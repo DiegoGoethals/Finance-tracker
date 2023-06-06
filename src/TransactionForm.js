ca1fdae3c3d3e695a7328d818e906b750e21779b
diego <goethalsdiego@gmail.com>
2023-06-06 23:00:03 +0200

    Fixed bugs where different component styles overlapped

--- src/TransactionForm.js
+++ src/TransactionForm.js
@@ -1,19 +1,28 @@
+import './TransactionForm.css';
+
 function TransActionForm(props) {
 
     const {handleSubmit, categories} = props;
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" name="amount" min="0" required/>
-            <label htmlFor="category">Category</label>
-            <select name="category" id="category">
+        <form onSubmit={handleSubmit} className='transaction'>
+            <h3>Add a transaction for today</h3>
+            <fieldset>
+                <label htmlFor="amount">Amount</label>
+                <input type="number" id="amount" name="amount" min="0" required/>
+            </fieldset>
+            <fieldset>
+                <label htmlFor="category">Category</label>
+                <select name="category" id="category">
                 {categories && categories.map(category => <option key={category.id} value={category.name}>{category.name}</option>)}
             </select>
-            <input type="radio" id="expense" name="type" value="expense" defaultChecked/>
-            <label htmlFor="expense">Expense</label>
-            <input type="radio" id="income" name="type" value="income"/>
-            <label htmlFor="income">Income</label>
+            </fieldset>
+            <fieldset className='radio'>
+                <input type="radio" id="expense" name="type" value="expense" defaultChecked/>
+                <label htmlFor="expense">Expense</label>
+                <input type="radio" id="income" name="type" value="income"/>
+                <label htmlFor="income">Income</label>
+            </fieldset>
             <button type="submit">Add</button>
         </form>
     );

23bb9e715d2e1ec0339f80047e8ffaabae321717
diego <goethalsdiego@gmail.com>
2023-06-06 12:38:03 +0200

    Added button and functionality to delete an account

--- src/AccountCard.js
+++ src/AccountCard.js
@@ -3,15 +3,26 @@ import "./AccountCard.css";
 
 function AccountCard(props) {
 
-    const { account } = props;
+    const { account, id } = props;
     const { name, balance } = account;
     let balanceClass = "account-balance";
     if (balance < 0) {
         balanceClass += " negative";
     }
 
+    const deleteAccount = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        fetch(`http://localhost:8000/accounts/${id}`, {
+            method: "DELETE",
+            }).then(() => {
+                window.location.reload();
+            });
+    }
+
     return (
-        <NavLink to={`/accounts/${props.id}`} className="account-card">
+        <NavLink to={`/accounts/${id}`} className="account-card">
+            <i className="fa-regular fa-circle-xmark" onClick={deleteAccount}></i>
             <div className="content-wrapper">
                 <p className="account-name">{name}</p>
                 <p className={balanceClass}>{balance}</p>
